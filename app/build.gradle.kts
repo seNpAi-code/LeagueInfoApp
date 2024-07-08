a546c955fb905c60be89ea16ca3f90c6df01c17d
Dinitha Nishadya <dinitha@cemexhrm.com>
2024-07-08 22:58:27 +0530

    with kapt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
-    id("com.google.devtools.ksp")
+    id ("kotlin-kapt")
 
 }
 
@@ -42,7 +42,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.2"
+        kotlinCompilerExtensionVersion = "1.5.2"
     }
     packaging {
         resources {
@@ -60,7 +60,7 @@ dependencies {
     implementation(libs.androidx.navigation.runtime.ktx)
     implementation(libs.androidx.media3.database)
     implementation(libs.androidx.material3.android)
-    ksp(libs.androidx.room.compiler)
+    kapt(libs.androidx.room.compiler)
     implementation(libs.androidx.room.ktx)
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
@@ -75,6 +75,6 @@ dependencies {
     implementation (libs.coil.compose)
 
 
-    annotationProcessor(libs.androidx.room.room.compiler)
+
 
 }
